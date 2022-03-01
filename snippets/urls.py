fa0f8558cb52114ca61d7ef7111eb1e082e08840
Murat <mrdudov@gmail.com>
2022-03-01 22:28:25 +0300

    use drf response add format_suffix_patterns

--- snippets/urls.py
+++ snippets/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 from snippets import views
+from rest_framework.urlpatterns import format_suffix_patterns
 
-urlpatterns = [
+urlpatterns = format_suffix_patterns([
     path('snippets/', views.snippet_list),
     path('snippets/<int:pk>/', views.snippet_detail),
-]
+])
